9cd1cc82d83eac71e6920c74491dc5fc28a344b5
Alexandru-Morteanu <clanzigzager@gmail.com>
2024-07-26 16:54:18 +0300

    de control

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,87 @@
-import { Elysia } from "elysia";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import Elysia from "elysia";
+import { spawn } from "child_process";
+import path from "path";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia();
+const httpServer = createServer(app); // Create HTTP server with Elysia app
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+let pythonProcess;
+const frameQueue = [];
+let processing = false;
+
+function startPythonProcess() {
+  const pythonScript = path.resolve(
+    "/Users/morteanualexandru/Desktop/pinbin/backend/py.py"
+  );
+  pythonProcess = spawn("python3", [pythonScript]);
+
+  console.log("Started Python process");
+
+  pythonProcess.on("close", (code) => {
+    console.log("Python script process exited with code", code);
+    pythonProcess = null;
+  });
+
+  pythonProcess.stdout.on("data", (data) => {
+    console.log("Python process output:", data.toString());
+    io.emit("detection", data.toString());
+    processing = false; // Reset processing state after receiving data
+    processNextFrame(); // Process the next frame in the queue
+  });
+
+  pythonProcess.stderr.on("data", (data) => {
+    console.error("Python error output:", data.toString());
+  });
+}
+
+function processNextFrame() {
+  if (frameQueue.length === 0 || !pythonProcess) {
+    processing = false;
+    return;
+  }
+
+  if (!processing) {
+    processing = true;
+
+    const frameData = frameQueue.shift();
+    const dataToSend = frameData.join(",");
+    // Send data to the Python process with a newline character
+    pythonProcess.stdin.write(dataToSend + "\n"); // Ensure newline for end of input
+  }
+}
+
+io.on("connection", (socket) => {
+  console.log("Client connected");
+
+  socket.on("frame", (frameData) => {
+    console.log("Received frame data");
+    frameQueue.push(frameData);
+    processNextFrame(); // Try to process the next frame
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+});
+
+process.on("exit", () => {
+  if (pythonProcess) {
+    pythonProcess.kill();
+  }
+});
+
+const port = 8088;
+httpServer.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+startPythonProcess(); // Start the Python process
